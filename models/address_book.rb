63e99826b02801827e9dfe9bcd7f1e91e8c46e66
Jared Truscott <Jtruscott78@gmail.com>
2017-11-30 18:57:38 -0500

    completed checkpoint 6

--- models/address_book.rb
+++ models/address_book.rb
@@ -11,7 +11,6 @@ class AddressBook
   def add_entry(name, phone_number, email)
     index = 0
     entries.each do |entry|
-
       if name < entry.name
         break
       end
@@ -22,17 +21,39 @@ class AddressBook
   end
 
   def remove_entry(name, phone_number, email)
-    entries.delete_if { |entry| name == entry.name &&
-    phone_number == entry.phone_number &&  email == entry.email }
+    entries.delete_if { |entry|
+      name == entry.name &&
+        phone_number == entry.phone_number && email == entry.email
+    }
   end
 
   def import_from_csv(file_name)
     csv_text = File.read(file_name)
-    csv = CSV.parse(csv_text, headers:true, skip_blanks: true)
+    csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
 
     csv.each do |row|
       row_hash = row.to_hash
       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
     end
+  end
+
+  def binary_search(name)
+    lower = 0
+    upper = entries.length - 1
+
+    while lower <= upper
+      mid = (lower + upper) / 2
+      mid_name = entries[mid].name
+
+      if name == mid_name
+        return entries[mid]
+      elsif name < mid_name
+        upper = mid - 1
+      elsif name > mid_name
+        lower = mid + 1
+      end
+    end
+
+    return nil
   end
 end
