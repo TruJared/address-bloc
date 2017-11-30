89ad073a316778703b281f9d8eb8e1af2e90e85e
Jared Truscott <Jtruscott78@gmail.com>
2017-11-30 13:41:39 -0500

    completed assignment 5

--- address_bloc.rb
+++ address_bloc.rb
@@ -3,6 +3,11 @@ require_relative 'controllers/menu_controller'
 menu = MenuController.new
 
 system "clear"
-puts "Welcome to AddressBloc"
+puts "Welcome to AddressBloc \n "
+
+puts"currently using sample entries"
+
+menu.address_book.add_entry('jared', '555', 'email daft')
+menu.address_book.add_entry('dale', 'pale', 'ale')
 
 menu.main_menu
--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -8,44 +8,52 @@ class MenuController
   end
 
   def main_menu
+    puts "\n"
     puts "Main Menu || #{address_book.entries.count} entries"
     puts "1) View all entries"
     puts "2) Create an entry"
     puts "3) Search for an entry"
     puts "4) Import entries from a CSV"
-    puts "5) Exit"
+    puts "5) View entry by position number"
+    puts "6) Exit"
     print "You must choose, but choose wisely: "
 
     selection = gets.to_i
     case selection
-      when 1
-        system 'clear'
-        view_all_entries
-        main_menu
-      when 2
-        system 'clear'
-        create_entry
-        main_menu
-      when 3
-        system 'clear'
-        search_entries
-        main_menu
-      when 4
-        system 'clear'
-        read_csv
-        main_menu
-      when 5
-        puts "Good-bye!"
-      else
-        system 'clear'
-        puts 'Invalid Option'
-        main_menu
+    when 1
+      system 'clear'
+      view_all_entries
+      main_menu
+    when 2
+      system 'clear'
+      create_entry
+      main_menu
+    when 3
+      system 'clear'
+      search_entries
+      main_menu
+    when 4
+      system 'clear'
+      read_csv
+      main_menu
+    when 5
+      system 'clear'
+      view_by_position
+    when 6
+      puts "Good-bye!"
+      exit(0)
+    else
+      system 'clear'
+      puts 'Invalid Option'
+      main_menu
     end
   end
 
   def view_all_entries
     address_book.entries.each do |entry|
       system "clear"
+
+      puts "#: #{address_book.entries.index(entry) + 1}"
       puts entry.to_s
       entry_submenu(entry)
     end
@@ -76,6 +84,17 @@ class MenuController
   def read_csv
   end
 
+  def view_by_position
+    system "clear"
+    print "You currently have #{address_book.entries.count} entries\n"
+    print "Position number to retrieve: "
+    position_number = (gets.chomp.to_i) - 1
+
+    puts "\n"
+    puts address_book.entries[position_number] if position_number >= 0 && position_number < address_book.entries.length
+    main_menu
+  end
+
   def entry_submenu(entry)
     puts "n - next entry"
     puts "d - delete entry"
@@ -85,16 +104,16 @@ class MenuController
     selection = gets.chomp
 
     case selection
-      when "n"
-      when "d"
-      when "e"
-      when "m"
-        system "clear"
-        main_menu
-      else
-        system "clear"
-        puts "#{selection} is not a valid input"
-        entry_submenu(entry)
+    when "n"
+    when "d"
+    when "e"
+    when "m"
+      system "clear"
+      main_menu
+    else
+      system "clear"
+      puts "#{selection} is not a valid input"
+      entry_submenu(entry)
     end
   end
 end
