af68b7745f0df5af60226f149d2f81617b1e6dae
Jared Truscott <Jtruscott78@gmail.com>
2017-11-30 20:01:47 -0500

    completed checkpoint 8

--- address_bloc.rb
+++ address_bloc.rb
@@ -5,9 +5,4 @@ menu = MenuController.new
 system "clear"
 puts "Welcome to AddressBloc \n "
 
-puts"currently using sample entries"
-
-menu.address_book.add_entry('jared', '555', 'email daft')
-menu.address_book.add_entry('dale', 'pale', 'ale')
-
 menu.main_menu
